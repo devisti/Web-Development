77798dbc43b4855b573cc66d6efa8b4ddd9ba991
Isti Hoque <115354997+devisti@users.noreply.github.com>
2025-05-29 16:27:12 +0530

    practice

---
+++ Practice/Practice 1/script.js
@@ -0,0 +1,9 @@
+console.log("Begining of the script.");
+console.log('12 x 12 =', 12*12);
+
+
+
+console.log("for loop starting below")
+for (let i = 1; i < 11; i++) {         // for loop testing.
+    console.log('2 x',i,'=',2*i);
+}
