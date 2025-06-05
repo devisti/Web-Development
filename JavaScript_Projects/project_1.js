c63d3bbf482587fbfb6ef1a794c4b95655b57044
Isti Hoque <115354997+devisti@users.noreply.github.com>
2025-06-05 16:35:07 +0530

    u

---
+++ JavaScript_Projects/project_1.js
@@ -0,0 +1,26 @@
+console.log('hello world')
+console.log('hello world')
+console.log('hello world')
+console.log('hello world')
+console.log('hello world')
+console.log('hello world')
+console.log('hello world')
+console.log('hello world')
+console.log('hello world')
+console.log('hello world')
+
+
+
+for (let i = 1; i < 100000; i++) {      //  'for loop' testing.
+    
+    console.log("2 x",i,"=",i*2);
+}
+
+
+
+let b = 1;
+
+while (b < 100) {                       // 'while loop' testing.
+    console.log("I Love You",b);
+    b++;
+}
